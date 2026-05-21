1ecb3ed83de203fe681f9e2261044de42bd356f0
Raseena99 <raseenacp24@gmail.com>
2026-05-22 00:15:28 +0530

    Update skill_matcher.py

--- app/skill_matcher.py
+++ app/skill_matcher.py
@@ -1,29 +1,40 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
 REQUIRED_SKILLS = [
-"python",
-"fastapi",
-"sql",
-"docker",
-"aws",
-"machine learning",
-"git",
-"rest api"
+    "python",
+    "fastapi",
+    "sql",
+    "docker",
+    "aws",
+    "machine learning",
+    "git",
+    "rest api"
 ]
+
 def find_matching_skills(resume_text: str):
-resume_text = resume_text.lower()
-matched = []
-missing = []
-for skill in REQUIRED_SKILLS:
-if skill in resume_text:
-matched.append(skill)
-else:
-missing.append(skill)
-return matched, missing
+    resume_text = resume_text.lower()
+    
+    matched = []
+    missing = []
+    
+    for skill in REQUIRED_SKILLS:
+        if skill in resume_text:
+            matched.append(skill)
+        else:
+            missing.append(skill)
+            
+    return matched, missing
+
 def calculate_resume_score(resume_text: str):
-job_description = " ".join(REQUIRED_SKILLS)
-documents = [resume_text, job_description]
-vectorizer = CountVectorizer()
-matrix = vectorizer.fit_transform(documents)
-similarity = cosine_similarity(matrix)[0][1]
-return round(similarity * 100, 2)
+    job_description = " ".join(REQUIRED_SKILLS)
+    
+    documents = [resume_text, job_description]
+    
+    vectorizer = CountVectorizer()
+    
+    matrix = vectorizer.fit_transform(documents)
+    
+    similarity = cosine_similarity(matrix)[0][1]
+    
+    return round(similarity * 100, 2)
