9110c24d4752e9784779e5f4e31aa293b2a0f04a
Raseena99 <raseenacp24@gmail.com>
2026-05-22 00:13:44 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, Integer, String, Float
 from .database import Base
+
 class ResumeAnalysis(Base):
-__tablename__ = "resume_analysis"
-id = Column(Integer, primary_key=True, index=True)
-candidate_name = Column(String)
-email = Column(String)
-score = Column(Float)
-matched_skills = Column(String)
-missing_skills = Column(String)
+    __tablename__ = "resume_analysis"
+
+    id = Column(Integer, primary_key=True, index=True)
+    candidate_name = Column(String)
+    email = Column(String)
+    score = Column(Float)
+    matched_skills = Column(String)
+    missing_skills = Column(String)
