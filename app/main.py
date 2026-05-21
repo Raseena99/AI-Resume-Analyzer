a4c9f354d75ab871e233187464921a2c826d0a08
Raseena99 <raseenacp24@gmail.com>
2026-05-21 17:39:25 +0530

    Initialize FastAPI application for resume analysis

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from .database import engine
+from .database import Base
+from .routes import router
+Base.metadata.create_all(bind=engine)
+app = FastAPI(
+title="AI Resume Analyzer",
+description="Analyze resumes using Python and AI techniques",
+version="1.0.0"
+)
+app.include_router(router)
+@app.get("/")
+def home():
+return {
+"message": "AI Resume Analyzer API Running"
+}
